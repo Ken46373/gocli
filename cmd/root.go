1adac8c34654bca65f7f4c70891896e43b335a77
Ken Minami <kenminami.dev@gmail.com>
2021-01-15 23:23:34 +0900

    Update root.go
    ファイル書き出しテスト

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,7 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -11,11 +11,35 @@ var RootCmd = &cobra.Command{
 	Use:   "igmaker",
 	Short: "gitignore maker by command line",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("root command")
+		if err := writeBytes("/Users/kenminami/test.txt"); err != nil {
+			os.Exit(1)
+		}
 	},
 }
 
 // Execute root command
 func Execute() {
 	RootCmd.Execute()
+}
+
+var (
+	lines = []string{"output test\n", "1234567890\n"}
+)
+
+// writeBytes write bytes to target file
+func writeBytes(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for _, line := range lines {
+		b := []byte(line)
+		_, err := file.Write(b)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
