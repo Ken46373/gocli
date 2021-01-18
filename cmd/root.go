7d8215b99fe6e2323b4c4686cedb5a2ff87ffda1
Ken Minami <kenminami.dev@gmail.com>
2021-01-18 23:00:05 +0900

    引数利用を実装中

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
 	"gocli/models"
@@ -12,10 +14,17 @@ import (
 var RootCmd = &cobra.Command{
 	Use:   "igmaker",
 	Short: "gitignore maker by command line",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := writeBytes("/Users/kenminami/test.txt"); err != nil {
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("requires language name used in your project")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := writeBytes("/Users/kenminami/test.txt", args); err != nil {
 			os.Exit(1)
 		}
+		return nil
 	},
 }
 
@@ -25,12 +34,13 @@ func Execute() {
 }
 
 // writeBytes write bytes to target file
-func writeBytes(filename string) error {
+func writeBytes(filename string, args []string) error {
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
+	fmt.Println(len(args))
 
 	for _, line := range models.Golang {
 		b := []byte(line)
